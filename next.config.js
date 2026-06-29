af59de72718280b70346d01f94ded5c61c88f238
agungraharjo-design <agungraharjo-design@users.noreply.github.com>
2026-06-29 21:12:10 +0700

    fix: also externalize @sparticuz/chromium in webpack for API routes

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 const nextConfig = {
   images: { unoptimized: true },
   serverComponentsExternalPackages: ['@sparticuz/chromium'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '@sparticuz/chromium']
+    }
+    return config
+  },
 }
 module.exports = nextConfig
